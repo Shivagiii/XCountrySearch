a3e2904699b4b736b8cd161a35f33a03d4711bfd
Shivagiii <shivangiadeshara@gmail.com>
2024-02-29 16:28:28 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [dataS, setDataS] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const fetchData = async () => {
+    //let url='https://restcountries.com/v3.1/all'
+
+    try {
+      const { data } = await axios.get("https://restcountries.com/v3.1/all");
+      console.log(data);
+      setData(data);
+      setDataS(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    let newData = data.filter((country) =>
+      country.name.common.toLowerCase().includes(search.toLowerCase())
+    );
+    console.log(newData);
+    setDataS(newData);
+  }, [search]);
+
+  function handleChange(e) {
+    setSearch(e.target.value);
+    console.log(search);
+    //const name=set.add(data.name.official);
+    //console.log(name)
+  }
+
+  // <input value={search} onChange={handleChange}/>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.input} >
+      <input value={search} onChange={handleChange} placeholder="Search for countries..." 
+      />
+      <br />
+      <div className={styles.container}>
+        {dataS.map((country) => (
+          <div className={styles.countryCard} key={country.cca3}>
+            <img alt={`Flag of ${country.flags.alt}`} src={country.flags.png} />
+            <h2>{country.name.common}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
